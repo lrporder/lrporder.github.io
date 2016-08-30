b979c13f538f5e32e1a684833761776bf59825c0
lrporder <lrporder@users.noreply.github.com>
2016-08-30 15:54:02 +0200

    KB enabled
    kerkbalans mogelijk

--- js/LRP-bestelling.js
+++ js/LRP-bestelling.js
@@ -311,7 +311,7 @@
         $('.Help').hide();
         
         // acties uitschakelen totdat het bestelbaar is
-        $('input#ActieNaamKerkbalans').prop('disabled', true);
+        // $('input#ActieNaamKerkbalans').prop('disabled', true);
         // $('input#ActieNaamEJC').prop('disabled', true);
         
         //===========================
